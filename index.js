a9b1ac7294922b3f6bd6e4ebf1fe7a3e59ae58ae
ajr-zimmer <ajr.zimmer@gmail.com>
2018-09-11 00:26:07 -0600

    [master] Made a diagonal line on the canvas

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+console.log('Hello World! 🌎');
+
+let canvas = document.getElementById('portal');
+let context = canvas.getContext('2d');
+
+let innerWidth = window.innerWidth;
+let devicePixelRatio = window.devicePixelRatio;
+canvas.width = innerWidth * devicePixelRatio;
+canvas.height = innerWidth * devicePixelRatio;
+context.scale(devicePixelRatio, devicePixelRatio);
+
+context.lineCap = 'square';
+context.lineWidth = 2;
+
+let manifest = { canvas, context, innerWidth, devicePixelRatio };
+console.log('Manifest:', manifest);
+
+const draw = (x, y, width, height) => {
+  context.moveTo(x, y);
+  context.lineTo(x + width, y + height);
+  context.stroke();
+};
+
+draw(0, 0, innerWidth, innerWidth);
