28ad047172abe5b1a849f8a35901eef71ce7f531
ajr-zimmer <ajr.zimmer@gmail.com>
2018-09-11 00:57:23 -0600

    [master] made some art

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ console.log('Hello World! 🌎');
 let canvas = document.getElementById('portal');
 let context = canvas.getContext('2d');
 
-let innerWidth = window.innerWidth;
-let devicePixelRatio = window.devicePixelRatio;
+// const innerWidth = window.innerWidth;
+const innerWidth = 320;
+const devicePixelRatio = window.devicePixelRatio;
 canvas.width = innerWidth * devicePixelRatio;
 canvas.height = innerWidth * devicePixelRatio;
 context.scale(devicePixelRatio, devicePixelRatio);
@@ -12,13 +13,30 @@ context.scale(devicePixelRatio, devicePixelRatio);
 context.lineCap = 'square';
 context.lineWidth = 2;
 
-let manifest = { canvas, context, innerWidth, devicePixelRatio };
+const manifest = { canvas, context, innerWidth, devicePixelRatio };
 console.log('Manifest:', manifest);
 
 const draw = (x, y, width, height) => {
-  context.moveTo(x, y);
-  context.lineTo(x + width, y + height);
+  const leftToRight = Math.random() >= 0.5;
+
+  if (leftToRight) {
+    context.moveTo(x, y);
+    context.lineTo(x + width, y + height);
+  } else {
+    context.moveTo(x + width, y);
+    context.lineTo(x, y + height);
+  }
+
+  context.strokeStyle = '#5C72B0';
   context.stroke();
 };
 
-draw(0, 0, innerWidth, innerWidth);
+const step = 25;
+
+for (let x = 0; x < innerWidth; x += step) {
+  for (let y = 0; y < innerWidth; y += step) {
+    draw(x, y, step, step);
+  }
+}
+
+// draw(0, 0, innerWidth, innerWidth);
